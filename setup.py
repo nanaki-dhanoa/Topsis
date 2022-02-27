71fb00622b9f30278cfb8f243cc7db5fe28f77d9
nanaki-dhanoa <68686571+nanaki-dhanoa@users.noreply.github.com>
2022-02-27 23:38:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='Topsis-Nanaki-101903195',  
-    version='0.1.0',
+    version='0.1.1',
     author="Nanaki",
     license='MIT',
     author_email="nanakidhanoa@gmail.com",
